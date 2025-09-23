ee9a8bec8b42094b6c19fc4bc9a53bf0f3d3edf4
kimnamheeee <nami200284@snu.ac.kr>
2025-09-23 21:30:16 +0900

    ✨ feat(usegamecontrols): add keyboard control logic using custom hook

---
+++ src/hooks/useGameControls.ts
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import useGame from './useGame';
+
+export default function useGameControls() {
+  const { moveBoard } = useGame();
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case 'ArrowUp':
+        case 'w':
+        case 'W':
+          moveBoard('up');
+          break;
+        case 'ArrowDown':
+        case 's':
+        case 'S':
+          moveBoard('down');
+          break;
+        case 'ArrowLeft':
+        case 'a':
+        case 'A':
+          moveBoard('left');
+          break;
+        case 'ArrowRight':
+        case 'd':
+        case 'D':
+          moveBoard('right');
+          break;
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [moveBoard]);
+}
