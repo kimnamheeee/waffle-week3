1d2f03b6d62a8ace511ec3745fa85b85ac564968
kimnamheeee <nami200284@snu.ac.kr>
2025-09-24 18:16:16 +0900

    ✨ feat(gamebutton): add variant to gamebutton

--- src/components/GameButton.tsx
+++ src/components/GameButton.tsx
@@ -3,21 +3,23 @@ import styled from 'styled-components';
 interface GameButtonProps {
   children: React.ReactNode;
   onClick: () => void;
+  variant?: 'primary' | 'ghost';
 }
 
-const Container = styled.button`
+const Container = styled.button<{variant: 'primary' | 'ghost'}>`
     border-radius: 10px;
     display: flex;
     align-items: center;
     justify-content: center;
-    color: white;
-    background-color: #998C7E;
+    color: ${({ variant }) => (variant === 'primary' ? 'white' : '#998C7E')};
+    background-color: ${({ variant }) => (variant === 'primary' ? '#998C7E' : 'transparent')};
+    border: ${({ variant }) => (variant === 'primary' ? 'none' : '2px solid #998C7E')};
     padding: 8px 16px;
     font-size: 16px;
     min-width: 116px;
     min-height: 40px;
 `;
 
-export default function GameButton({ children, onClick }: GameButtonProps) {
-  return <Container onClick={onClick}>{children}</Container>;
+export default function GameButton({ children, onClick, variant = 'primary' }: GameButtonProps) {
+  return <Container onClick={onClick} variant={variant}>{children}</Container>;
 }
